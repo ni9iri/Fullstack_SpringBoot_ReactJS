ef2a73ba101125c0862c430ebf5290f8dde35ffa
Quyen Nguyen <quyen.ng1902@gmail.com>
2023-03-08 18:31:14 +0200

    Added Appbar using Material UI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Appbar from "./components/Appbar";
+
+function App() {
+  return (
+    <div>
+      <Appbar />
+      <h1>This is my app</h1>
+    </div>
+  );
+}
+
+export default App;
